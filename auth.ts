fa182e726982a5ac0cd32788db533043e0ca71a2
moghaleb <127904494+moghaleb@users.noreply.github.com>
2026-02-28 02:14:37 +0300

    Fix NextAuth error

--- auth.ts
+++ auth.ts
@@ -96,4 +96,5 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         },
     },
     secret: "f4d4a8e2b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b2c3d4e5",
+    trustHost: true,
 });
